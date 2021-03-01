d7835369140c044a488e56e11d97e1367c83f529
Kretinoh <Kretinoh@192.168.1.128>
2021-03-02 00:03:45 +0100

    Update Array03

--- Programacion/src/Array1/Array03.java
+++ Programacion/src/Array1/Array03.java
@@ -1,10 +1,23 @@
 package Array1;
 
+import java.util.Scanner;
+
 public class Array03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner s = new Scanner(System.in);
+		
+		int[] array = new int[10]; 
+		
+		for (int i = 0; i < array.length; i++) {
+			System.out.print("Introduzca un numero para el array > ");
+			
+			array[i] = s.nextInt();
+		}
+		for (int i = array.length -1; i >= 0; i--) {
+			System.out.println(array[i]);
+		}
 	}
 
 }
