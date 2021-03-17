1f4bd5e9aae5f12e5a92365101531282d75fe906
Kr371n0h <48481680+kretinoh@users.noreply.github.com>
2021-03-17 15:59:57 +0100

    Added QuitaComentarios - Final version

--- Programacion/src/ficherosEjercicios/QuitaComentarios.java
+++ Programacion/src/ficherosEjercicios/QuitaComentarios.java
@@ -1,49 +1,69 @@
 package ficherosEjercicios;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
+import java.io.IOException;
 
 /**
  * Quitar comentarios de un fichero
  * 
  * @author KR371N0H
  *
- *
+ * 
  */
 public class QuitaComentarios {
 
   public static void main(String[] args) {
+    String linea;
+    boolean comentarioBloque = false;
+
+    /**
+     * Comprobamos que se nos pasen 2 parámetros en el programa
+     */
+    if (args.length != 2) {
+      System.err.println("Error en el numero de parámetros");
+      System.exit(1);
+    }
 
     try {
+      // Abrimos lectura del fichero 0
+      BufferedReader file = new BufferedReader(new FileReader(args[0]));
+
+      // Creamos escritura argumento 1
       BufferedWriter destinationFile = new BufferedWriter(new FileWriter(args[1]));
-      // Leemos todas las lineas del fichero pasado por parámetro y la guardamos en allLines
-      List<String> allLines = Files.readAllLines(Paths.get(args[0]));
 
-      // Guardamos cada linea del fichero en cada iteracion en la variable linea
-      for (String linea : allLines) {
-        if (!((linea.contains("/**")) || (linea.contains("//")))) {
-          destinationFile.write(linea);
-        } else if (linea.contains("//")) {
-          // Con substring cogemos desde el inicio hasta donde se encuentra el simbolo "//"
-          linea = linea.substring(0, linea.indexOf("//"));
-          destinationFile.write(linea);
+      while ((linea = file.readLine()) != null) {
+        // Buscamos comentarios de bloque
+        if (linea.indexOf("/*") != -1) {
+          comentarioBloque = true;
         }
-        // else if (!linea.contains("/* ")) {
-        //
-        // destinationFile.write(linea);
-        // }
-        // Salto de linea
-        destinationFile.newLine();
-      }
-      destinationFile.close();
 
-    } catch (Exception e) {
-      System.out.println("No se ha podido abrir el archivo.");
+        // Buscamos el final del comentario
+        if (linea.indexOf("*/") != -1) {
+          comentarioBloque = false;
+          linea = linea.substring(linea.indexOf("/") + 1, linea.length());
+        }
+
+        // Buscamos comentarios de linea
+        if (linea.indexOf("//") != -1) {
+          linea = linea.substring(0, linea.indexOf("/") - 1);
+        }
+
+        if (!comentarioBloque) {
+          destinationFile.write(linea);
+          destinationFile.newLine();
+        }
+      }
+      file.close();
+      destinationFile.close();
+    } catch (IOException e) {
+      
+      System.out.println("No se ha podido abrir el archivo o no se encontró el fichero.");
     }
 
   }
+
 
 }
