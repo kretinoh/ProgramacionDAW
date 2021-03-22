a409e9085e21645ade543c7e89babb4d2e2fbbec
Kr371n0h <48481680+kretinoh@users.noreply.github.com>
2021-03-22 17:44:26 +0100

    Main Gestesimal

---
+++ Programacion/src/gestisimal/TestAlmacen.java
@@ -0,0 +1,14 @@
+package gestisimal;
+
+public class TestAlmacen {
+
+  public static void main(String[] args) {
+    // GENERAMOS UN ALMACEN
+    Almacen almacen = new Almacen();
+
+    // MENU
+    
+
+  }
+
+}
