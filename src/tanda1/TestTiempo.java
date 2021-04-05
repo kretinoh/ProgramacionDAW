ab9a782dd84152e0cad65e406666c239d99007d0
Kr371n0h <48481680+kretinoh@users.noreply.github.com>
2021-04-05 15:15:28 +0200

    TestTiempo sin terminar

---
+++ src/tanda1/TestTiempo.java
@@ -0,0 +1,40 @@
+package tanda1;
+
+import java.util.Scanner;
+
+public class TestTiempo {
+
+  public static void main(String[] args) {
+  Scanner s = new Scanner(System.in);
+  
+  System.out.println("Vamos a crear el Tiempo1\nIntroduce las horas,minutos y segundos: ");
+  int horas = s.nextInt();
+  int minutos = s.nextInt();
+  int segundos = s.nextInt();
+  
+  Tiempo t1 = new Tiempo(horas,minutos,segundos);
+  Tiempo t2 = new Tiempo(5,20,15);
+  
+  System.out.println("Tiempo 1 > " + t1.toString());
+  System.out.println("Tiempo 2 > " + t2 + "\n");
+  
+//  Sumarle otro objeto
+  Tiempo suma = t1.sumarTiempo(t2);
+  System.out.println(t1 + " + " + t2 + " = " + suma);
+  
+//  Restarle un tiempo
+  Tiempo resta = t1.restarTiempo(t2);
+  System.out.println(t1 + " - " + t2 + " = " + resta);
+  
+//  Sumar segundos
+  System.out.print(t1 + " + 23 segundos =");
+  t1.sumarSegundos(23);
+  System.out.println(t1);
+  
+//  Restar segundos
+  System.out.print(t1 +" - 23 segundos =");
+  t1.restarSegundos(23);
+  System.out.println(t1);
+  }
+
+}
