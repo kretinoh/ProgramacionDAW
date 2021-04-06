da27019db15f51e932455dbca306d044ea0cdf96
Kr371n0h <48481680+kretinoh@users.noreply.github.com>
2021-04-06 15:16:30 +0200

    Test tiempo tanda 1 finalizado

--- src/tanda1/TestTiempo.java
+++ src/tanda1/TestTiempo.java
@@ -31,10 +31,30 @@ public class TestTiempo {
   t1.sumarSegundos(23);
   System.out.println(t1);
   
+//  Sumar minutos
+  System.out.print(t1 + " + 5 minutos  = ");
+  t1.sumarMinutos(5);
+  System.out.println(t1);
+  
+//  Sumar horas
+  System.out.println(t1 + " 5 horas = ");
+  t1.sumarHoras(5);
+  System.out.println(t1);
+  
 //  Restar segundos
   System.out.print(t1 +" - 23 segundos =");
   t1.restarSegundos(23);
   System.out.println(t1);
+  
+  
+//  Restar minutos
+  System.out.print(t1 + " - 5 minutos = ");
+  t1.restarMinutos(5);
+  System.out.println(t1);
+  
+//  Restar horas
+  System.out.print(t1 + " - 2 horas = ");
+  t1.restarHoras(2);
+  System.out.println(t1);
   }
-
 }
